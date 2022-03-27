5fdc257467e19a753c1c839d1731c60707aa2d64
mtehila <tehila1633@gmail.com>
2022-03-27 14:21:21 +0300

    userSchema

---
+++ models/users.model.js
@@ -0,0 +1,23 @@
+const {  mongoose } = require("mongoose")
+const UsersSchema=new mongoose.Schema(
+    {
+        user_first:String,
+        user_last_name:String,
+        user_phone:String,
+        arr_orders:[OrderSchema]
+    }
+)
+
+const Users=mongoose.model("users",UsersSchema)
+module.exports={Users,UsersSchema}
+
+const OrderSchema=new mongoose.Schema(
+    {
+        item_id:String,
+        item_name:String,
+        item_description:String,
+        price:Number
+    }
+)
+const Order=mongoose.model("orders",OrderSchema)
+module.exports={Order,OrderSchema}
