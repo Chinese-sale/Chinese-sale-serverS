857abd5eb05390b790dc2ee12ce68fbc3665ffe1
shoshiShrayber <96003594+shoshiShrayber@users.noreply.github.com>
2022-03-27 18:22:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const usersRout = require("./routes/users");
-const app = express();
+const itemRout = require("./routes/item");
 
+const app = express();
 mongoose.connect("mongodb://localhost:27017/Chinese-sale-serverS")
     .then(() => { console.log("mongo db connected"); })
     .catch(er => { console.log(er) });
 
 app.use(express.json())
 app.use("/users", usersRout)
+app.use("/item", itemRout)
 app.listen(4500, () => { console.log("waiting") })
-
