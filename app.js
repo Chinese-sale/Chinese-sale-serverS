448a1a0ad338f34c148db8c500ce0c3b5e22d7da
shoshiShrayber <96003594+shoshiShrayber@users.noreply.github.com>
2022-03-27 18:18:40 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const itemRout = require("./routes/item");
-const app = express();
-
-
-app.use(express.json())
-app.use("/item", itemRout)
