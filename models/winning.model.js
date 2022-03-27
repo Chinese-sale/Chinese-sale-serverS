97782f244211061890cbdb1176e953a87debb51b
TamarCoh <tc053316@gmail.com>
2022-03-27 17:27:17 +0300

    winning and winning.model

--- models/winning.model.js
+++ models/winning.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const authorSchema = new mongoose.Schema({
-  user_id:{ type: mongoose.Types.ObjectId, ref: "books"},
-  item_id:{ type: mongoose.Types.ObjectId, ref: "books"},
+  user_id:{ type: mongoose.Types.ObjectId, ref: "users"},
+  item_id:{ type: mongoose.Types.ObjectId, ref: "items"},
   winning_data:{ type: Date, default: Date.now() }
 });
 
-const Author = mongoose.model("authors", authorSchema);
-module.exports = { Author, authorSchema };
+const Winning = mongoose.model("Winning", authorSchema);
+module.exports = { Winning, WinningSchema };
